a606e69875f22ac59588bfcb01a6e0905037bb44
Brasko91 <wotb.jr2@gmail.com>
2024-06-02 21:31:29 +0200

    nsi

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mettre un style Special
+function mettreStyleSpecial() {
+  document.getElementById("p").classList.add('special');
+}
+
+// Enlever le style Special
+function enleveStyleSpecial() {
+  document.getElementById("p").classList.remove('special');
+}
+
